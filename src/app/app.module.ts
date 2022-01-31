781f1bfda3a312a1a1106034ff092f8c9452760b
Maicol Jimenez <maicol_jimenez82121@elpoli.edu.co>
2022-01-30 20:55:32 -0500

    primera copia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// routs
+import { APP_ROUTES } from 'src/app/app.roouts';
+// components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopageComponent } from './nopage/nopage.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SidebarComponent,
+    HomeComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    Graficas1Component,
+    ProgressComponent,
+    DashboardComponent,
+    NopageComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
